c4210394c48dac3a3b07337c772d0e5ced189916
taroponta333 <taroponta.333@gmail.com>
2026-07-06 16:16:12 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,15 +5,15 @@
 #include <pspnet_apctl.h>
 #include <pspnet_resolver.h>
 #include <psputility.h>
-#include <psputility_msgdialog.h> // 追加
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/select.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <pspctrl.h> // ✨ ボタン入力を監視するために追加
 
+// モジュール情報をカーネル寄りに変更して安定化
 PSP_MODULE_INFO("3DS_Receiver", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
@@ -23,13 +23,29 @@ PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
 int exit_request = 0;
 
-// 終了確認ダイアログ関数
+// 標準の終了コールバック（HOMEメニュー用、ダイアログなしで即終了する安全版）
+int exit_callback(int arg1, int arg2, void *common) {
+    exit_request = 1;
+    return 0;
+}
+int callback_thread(SceSize args, void *argp) {
+    int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+    sceKernelRegisterExitCallback(cbid);
+    sceKernelSleepThreadCB();
+    return 0;
+}
+void setup_callbacks(void) {
+    int thid = sceKernelCreateThread("update_thread", callback_thread=, 0x11, 0xFA0, 0, 0);
+    if (thid >= 0) sceKernelStartThread(thid, 0, 0);
+}
+
+// アプリ内で「本当に終了しますか？」を出す安全な関数
 int show_exit_dialog(void) {
     pspUtilityMsgDialogParams dialog;
     memset(&dialog, 0, sizeof(dialog));
     
-    dialog.base.size = sizeof(pspUtilityMsgDialogParams);
-    dialog.base.language = 1; 
+    dialog.base.size = sizeof(dialog);
+    dialog.base.language = 1;
     dialog.base.buttonSwap = 0;
     dialog.base.graphicsThread = 17;
     dialog.base.accessThread = 19;
@@ -37,10 +53,10 @@ int show_exit_dialog(void) {
     dialog.base.soundThread = 16;
     
     dialog.mode = PSP_UTILITY_MSGDIALOG_MODE_TEXT;
-    dialog.type = PSP_UTILITY_MSGDIALOG_TYPE_YESNO;
-    strcpy(dialog.message, "Do you want to quit the application?");
+    dialog.option = PSP_UTILITY_MSGDIALOG_OPTION_YESNO;
+    snprintf(dialog.message, 512, "Do you want to quit the application?");
 
-    sceUtilityMsgDialogInit(&dialog);
+    sceUtilityMsgDialogInitStart(&dialog);
 
     while (1) {
         int status = sceUtilityMsgDialogGetStatus();
@@ -49,32 +65,14 @@ int show_exit_dialog(void) {
         } else if (status == PSP_UTILITY_DIALOG_STATUS_FINISHED) {
             if (dialog.buttonPressed == PSP_UTILITY_MSGDIALOG_RESULT_YES) {
                 sceUtilityMsgDialogShutdownStart();
-                return 1;
+                return 1; // 終了する
             }
             sceUtilityMsgDialogShutdownStart();
-            return 0;
+            return 0; // 戻る
         }
-        sceKernelDelayThread(10000);
+        // ループの暴走を防ぐために長めのウェイトを入れる
+        sceDisplayWaitVblankStart(); 
     }
-}
-
-int exit_callback(int arg1, int arg2, void *common) {
-    if (show_exit_dialog()) {
-        exit_request = 1;
-    }
-    return 0;
-}
-
-int callback_thread(SceSize args, void *argp) {
-    int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
-    sceKernelRegisterExitCallback(cbid);
-    sceKernelSleepThreadCB();
-    return 0;
-}
-
-void setup_callbacks(void) {
-    int thid = sceKernelCreateThread("update_thread", callback_thread, 0x11, 0xFA0, 0, 0);
-    if (thid >= 0) sceKernelStartThread(thid, 0, 0);
 }
 
 int init_network(void) {
@@ -114,7 +112,8 @@ int main(void) {
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-    struct timeval timeout = {3, 0};
+    // 通信のタイムアウトを短くしてループを回しやすくする
+    struct timeval timeout = {0, 500000}; // 0.5秒
     setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
     char buffer[PACKET_SIZE];
@@ -123,16 +122,35 @@ int main(void) {
     FILE *file = NULL;
     int is_transferring = 0;
 
+    SceCtrlData pad;
+
+    printf("Waiting for 3DS broadcast... (Press TRIANGLE to quit)\n");
+
     while (!exit_request) {
+        // ✨ ボタン入力を監視（毎ループチェック）
+        sceCtrlPeekBufferPositive(&pad, 1);
+        if (pad.buttons & PSP_CTRL_TRIANGLE) { // △ボタンが押されたら
+            if (show_exit_dialog()) {
+                break; // ループを抜けて安全に終了
+            } else {
+                printf("Waiting for 3DS broadcast... (Press TRIANGLE to quit)\n");
+            }
+        }
+
         int bytes_received = recvfrom(sock, buffer, PACKET_SIZE, 0, (struct sockaddr *)&client_addr, &addr_len);
         if (bytes_received > 0) {
             if (!is_transferring) {
                 file = fopen(SAVE_PATH, "wb");
                 is_transferring = 1;
+                printf("Receiving data...\n");
             }
-            fwrite(buffer, 1, bytes_received, file);
+            if (file) {
+                fwrite(buffer, 1, bytes_received, file);
+                printf(".");
+            }
         } else if (is_transferring) {
-            fclose(file);
+            if (file) fclose(file);
+            printf("\nTransfer complete!\n");
             break;
         }
     }
