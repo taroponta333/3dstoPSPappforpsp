3882f0ce12aa1a2d45d8eab4774ce70543f94923
taroponta333 <taroponta.333@gmail.com>
2026-07-08 19:22:14 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,26 @@
 #include <pspkernel.h>
 #include <pspdebug.h>
-#include <psputility.h>
-#include <psputility_netconf.h>
-#include <pspnet.h>
-#include <pspnet_inet.h>
-#include <pspnet_apctl.h>
-#include <pspnet_resolver.h>
 #include <pspctrl.h>
-#include <pspdisplay.h>
-#include <stdio.h>
-PSP_MODULE_INFO("3DS_App_Receiver",0,1,0);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER|THREAD_ATTR_VFPU);
-int main(void){
- pspDebugScreenInit();
- printf("3DS App Receiver\n");
- printf("Skeleton source generated.\n");
- sceKernelExitGame();
- return 0;
+
+PSP_MODULE_INFO("HelloPSP", 0, 1, 0);
+PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
+
+int main(void)
+{
+    pspDebugScreenInit();
+
+    pspDebugScreenPrintf("=================================\n");
+    pspDebugScreenPrintf("        3DS App Receiver\n");
+    pspDebugScreenPrintf("=================================\n\n");
+
+    pspDebugScreenPrintf("Hello PSP!\n");
+    pspDebugScreenPrintf("This program is running.\n\n");
+    pspDebugScreenPrintf("Press HOME to exit.\n");
+
+    while (1)
+    {
+        sceDisplayWaitVblankStart();
+    }
+
+    return 0;
 }
