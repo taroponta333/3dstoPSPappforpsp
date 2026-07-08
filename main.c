6d9592cbf9e28c3eaac08f6c47caf46de7f7bf85
taroponta333 <taroponta.333@gmail.com>
2026-07-08 19:31:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,11 +2,52 @@
 #include <pspdebug.h>
 #include <pspctrl.h>
 #include <pspdisplay.h>
+
 PSP_MODULE_INFO("HelloPSP", 0, 1, 0);
 PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER | THREAD_ATTR_VFPU);
 
+/* -------- HOMEボタン対応 -------- */
+
+int exit_callback(int arg1, int arg2, void *common)
+{
+    sceKernelExitGame();
+    return 0;
+}
+
+int CallbackThread(SceSize args, void *argp)
+{
+    int cbid;
+
+    cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+    sceKernelRegisterExitCallback(cbid);
+
+    sceKernelSleepThreadCB();
+
+    return 0;
+}
+
+int SetupCallbacks(void)
+{
+    int thid = sceKernelCreateThread(
+        "update_thread",
+        CallbackThread,
+        0x11,
+        0xFA0,
+        0,
+        NULL);
+
+    if (thid >= 0)
+        sceKernelStartThread(thid, 0, 0);
+
+    return thid;
+}
+
+/* ------------------------------ */
+
 int main(void)
 {
+    SetupCallbacks();
+
     pspDebugScreenInit();
 
     pspDebugScreenPrintf("=================================\n");
